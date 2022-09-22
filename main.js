e335f3c6144535da60047319559f96f4c4f39bec
sarayukatukam <86565311+sarayukatukam@users.noreply.github.com>
2022-09-22 18:09:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+peter_pan_song = "";
+Harry_Potter_Theme_song = "";
+
 function setup(){
     canvas = createCanvas(600,530);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video,0,0,600,530);
+}
+
+function preload() {
+    peter_pan_song = loadSound("music2.mp3");
+    Harry_Potter_Theme_song = loadSound("music.mp3");
 }
